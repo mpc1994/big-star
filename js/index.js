47a12a53692e7400a2395348631306278e36ddd5
mpc1994 <1370208676@qq.com>
2016-07-04 22:26:05 +0800

    九宫格1.2

--- js/index.js
+++ js/index.js
@@ -2,10 +2,10 @@ window.onload=function(){
 	var oBtn=document.getElementById("button");
 	var oMask=document.getElementById("mask-layer");
 	var oBtn2=document.getElementById("button2");
+	var oBtn1=document.getElementById("button1");
 	var oUl=document.getElementById("ul-wrap");
 	var oSpan=oUl.getElementsByTagName("span");
-	console.log(["oSpan",oSpan]);
-
+	var oCh=document.getElementById("changebox");
 	//显示遮盖层
 	oBtn.onclick=function(){
 		oMask.style.display="block";
@@ -20,7 +20,36 @@ window.onload=function(){
 
 	//第一个span按钮操作
 	oSpan[0].onclick=function(){
-		console.log("我是第一个span");
+       oCh.style.background="red";
+	}
+	oSpan[1].onclick=function(){
+       oCh.style.background="yellow";
+	}
+	oSpan[2].onclick=function(){
+       oCh.style.background="blue";
 	}
 
+ 	oSpan[3].onclick=function(){
+ 		oCh.style.width="200px";
+ 	}
+ 	oSpan[4].onclick=function(){
+ 		oCh.style.width="300px";
+ 	}
+ 	oSpan[5].onclick=function(){
+ 		oCh.style.width="400px";
+ 	}
+ 	oSpan[6].onclick=function(){
+ 		oCh.style.height="50px";
+ 	}
+ 	oSpan[7].onclick=function(){
+ 		oCh.style.height="100px";
+ 	}
+ 	oSpan[8].onclick=function(){
+ 		oCh.style.height="150px";
+ 	}
+ 	oBtn1.onclick=function(){
+ 		oCh.style.background="black";
+ 		oCh.style.width="200px";
+ 		oCh.style.height="200px";
+ 	}
 }
